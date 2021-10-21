b3b1a3cbed20874d2a11aac7d82c17523ffdb54d
mxopolokill <73064238+mxopolokill@users.noreply.github.com>
2021-10-21 13:38:34 +0200

    augmentation post page

--- controlleur/controlleurAPI.php
+++ controlleur/controlleurAPI.php
@@ -3,7 +3,7 @@
 
 
 $API = new APICALL;
-$API->url = "https://www.lecoledunumerique.fr/wp-json/wp/v2/apprenants";
+$API->url = "https://www.lecoledunumerique.fr/wp-json/wp/v2/apprenants?per_page=100";
 
 $Myjson = $API->curl1($API->url);
 
